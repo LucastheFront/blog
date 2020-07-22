419aa91fb86418484536a853592f6d46104c5c05
Lucas Besnard <lbesnard@openwt.com>
2020-07-22 18:13:32 +0200

    Add image and author to post

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,11 @@
 const posts = [
     {
         postId: 1,
+        imageSource: 'https://images.daznservices.com/di/library/GOAL/9/4f/jorge-jesus-flamengo-botafogo-carioca-07-03-2020_195az3ietwmc51bkgx8rpe1033.jpg?t=-1216696048&quality=100',
         title: 'Jorge Jesus, nouvel entraîneur du Benfica',
         description: "Après avoir quitté le Benfica Lisbonne à l'été 2015 pour rejoindre les rangs du rival du Sporting Portugal, Jorge Jesus est de retour au SLB. Si l'entraîneur portugais a pris quelques galons supplémentaires avec son passage réussi à Flamengo, les supporters du Benfica n'ont pas oublié l'acte de trahison du double J.",
         article: "La vie nous l'a appris, revenir dans les bras de son ex n’est que très rarement la meilleure solution. Et ce, même s’il existe quelques exceptions à l’image de Zinédine Zidane au Real Madrid, LeBron James à Cleveland ou encore Kate Middleton et le prince William. Car oui, comme l'explique le site psychologies.com, il existe toujours cette envie de revoir son ancien amour. Même si cela n'est pas forcément une bonne idée : « Certains couples se reforment. Ils reviennent au 'même', au 'connu', car c'est rassurant et confortable. Renouer avec quelqu’un est souvent plus simple au début. On connaît déjà les qualités et les défauts. On retrouve un noyau familial et qui nous a rendus heureux. Mais est-ce bien ce que l’on recherche ? (...) D'autre fois, lorsqu'on ne retrouve personne, il peut s'agir d'une décision prise par défaut. » Autant de questions que le président du Benfica Lisbonne, Luís Filipe Vieira, s’est peut-être posé au moment de choisir le futur entraîneur du SLB. Et alors que les noms d'Unai Emery ou encore Mauricio Pochettino se faisaient entendre dans les médias portugais, c’est finalement Jorge Jesus, passé au Benfica Lisbonne entre 2009 et 2015, qui a obtenu le job. Pour le meilleur et pour le pire ?",
+        author: 'Thomas',
         comments: [
             {
                 author: 'Lucas',
